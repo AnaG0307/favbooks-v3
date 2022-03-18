781300ca5547ba5df74b08cdf5ce1de2b54b8656
AnaG0307 <ana.genover@gmail.com>
2022-03-18 14:26:36 +0000

    Trying different development environment variable

--- favbooks_v3/settings.py
+++ favbooks_v3/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-v9tj!ok-mmy&&pb8va4fqgv#_-4sgjpib_4))vgh+gr1w=y91(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', os.environ.get('HEROKU_HOSTNAME'),]
 
