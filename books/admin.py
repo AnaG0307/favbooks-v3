e9ea1a186dd8e7805cc0e3bff3c9f8c7defa1418
AnaG0307 <ana.genover@gmail.com>
2022-04-01 11:39:24 +0000

    Corrected the plural of category and subcategory in the admin site and customised the admin site views for the different models

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
 from .models import Book, Category, Type, Comment, Sub_Category
 
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Category)
-admin.site.register(Sub_Category)
-admin.site.register(Type)
+
+# Class to show book information on the displayed list in the Admin Site
+# and order by author
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'author',
+        'title',
+        'book_category',
+        'book_sub_category',
+        'book_type',
+        'cover',
+    )
+
+    ordering = ('author',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'book_category',
+        'book_friendly_category',
+    )
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'book_sub_category',
+        'book_friendly_sub_category',
+    )
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'book_type',
+        'book_friendly_type',
+    )
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Sub_Category, SubCategoryAdmin)
+admin.site.register(Type, TypeAdmin)
 admin.site.register(Comment)
--- books/models.py
+++ books/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
+    """
+    Meta class to correct the "categorys" incorrect spelling in the admin site
+    """
+    class Meta:
+        verbose_name_plural = "Categories"
+
     book_category = models.CharField(max_length=254)
     book_friendly_category = models.CharField(
         max_length=254, null=True, blank=True
@@ -13,7 +19,15 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.book_friendly_category
 
+
 class Sub_Category(models.Model):
+
+    """
+    Meta class to correct the "categorys" incorrect spelling in the admin site
+    """
+    class Meta:
+        verbose_name_plural = "Sub_Categories"
+
     book_sub_category = models.CharField(max_length=254)
     book_friendly_sub_category = models.CharField(
         max_length=254, null=True, blank=True
@@ -53,10 +67,18 @@ class Book(models.Model):
     title = models.CharField(max_length=254)
     author = models.CharField(max_length=254)
     synopsis = models.TextField()
-    price_hardcover = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    price_paperback = models.DecimalField(max_digits=6, decimal_places=2)
-    price_electronic = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price_hardcover = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+        )
+    price_paperback = models.DecimalField(
+        max_digits=6, decimal_places=2
+        )
+    price_electronic = models.DecimalField(
+        max_digits=6, decimal_places=2
+        )
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+        )
     cover_url = models.URLField(max_length=1024, null=True, blank=True)
     cover = models.ImageField(null=True, blank=True)
 
