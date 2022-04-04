a3567f5be37ea73846cc2db665c5283839e643c9
AnaG0307 <ana.genover@gmail.com>
2022-04-04 18:02:18 +0000

    Added search functionality to search through title, author and synopsis

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 
@@ -6,9 +8,21 @@ def all_books(request):
     """ View to return all books, sorting and search queries """
 
     books = Book.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria entered!")
+                return redirect(reverse('books'))
+
+            queries = Q(title__icontains=query) | Q(synopsis__icontains=query) | Q(author__icontains=query)
+            books = books.filter(queries)
 
     context = {
         'books': books,
+        'search_term': query
     }
 
     return render(request, 'books/books.html', context)
