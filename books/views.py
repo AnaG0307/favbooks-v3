276f425b9cb3333d07351484774a6fa106107d14
AnaG0307 <ana.genover@gmail.com>
2022-04-04 11:12:43 +0000

    Created views and templates to show books information

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def all_books(request):
+    """ View to return return all books, sorting and search queries """
+
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'books/books.html', context)
