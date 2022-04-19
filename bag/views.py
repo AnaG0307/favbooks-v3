19543cabcadc78efc95061a3950bfdd0c92a5582
AnaG0307 <ana.genover@gmail.com>
2022-04-19 15:58:26 +0000

    Created toasts template folder and created templates for the different types of messages, also added messages to views in the bag app

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from books.models import Book
 
 
 def view_bag(request):
@@ -10,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ View to add quantity of a specific book to the shopping bag """
 
+    product = Book.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -18,6 +21,10 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(
+            request,
+            f'Added {product.title} by {product.author} to your bag'
+            )
 
     request.session['bag'] = bag
     return redirect(redirect_url)
