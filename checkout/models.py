de42b1e575883cf3b04c7084a3c0fad93cf7deae
AnaG0307 <ana.genover@gmail.com>
2022-04-26 10:40:32 +0000

    Added checkout success functionality

--- checkout/models.py
+++ checkout/models.py
@@ -38,7 +38,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
-            'lineitem_total__sum']
+            'lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * \
                 settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -76,7 +76,7 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.product.price_hardcover * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
