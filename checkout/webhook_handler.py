bb9ee20124b92ef6c0ab1f4692cbea1c45773a93
AnaG0307 <ana.genover@gmail.com>
2022-05-18 10:57:08 +0000

    Implemented purchase confirmation emils, webhook not working at the moment

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from books.models import Book
@@ -13,6 +16,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """
@@ -78,15 +98,12 @@ class StripeWH_Handler:
                     stripe_pid=pid,
                 )
                 order_exists = True
-                # webhook TLS issue
-                print('Testing string6')
                 break
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-            # webhook TLS issue
-            print('Testing string1')
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -115,19 +132,14 @@ class StripeWH_Handler:
                         product=product,
                         quantity=item_data,
                     )
-                    # webhook TLS issue
-                    print('Testing string7')
                     order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
-                # webhook TLS issue
-                print('Testing string2')
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        # webhook TLS issue
-        print('Testing string3')
+        self._send_confirmation_email(order)
         return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Created order in Webhook',
                 status=200)
@@ -136,8 +148,6 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.Payment.failed webhook from Stripe
         """
-        # webhook TLS issue
-        print('Testing string4')
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -27,11 +27,14 @@ def webhook(request):
             )
     except ValueError as e:
         # Invalid payload
+        print("valueerror", e)
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
+        print("sigerror", e)
         return HttpResponse(status=400)
     except Exception as e:
+        print("exception", e)
         return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
