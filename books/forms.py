b21726f0ac68aceaebaaba8b47487942c1da3afe
AnaG0307 <ana.genover@gmail.com>
2022-05-19 09:58:55 +0000

    Created forms.py in Books app and created logic for the form to be displayed for the admin user to be able to add books from the site

---
+++ books/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Book, Category, Sub_Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        sub_categories = Sub_Category.objects.all()
+        friendly_names_c = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_names_s = [(
+            s.id, s.get_friendly_name()
+            ) for s in sub_categories]
+
+        self.fields['category'].choices = friendly_names_c
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black'
+
+        self.fields['sub_category'].choices = friendly_names_s
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black'
