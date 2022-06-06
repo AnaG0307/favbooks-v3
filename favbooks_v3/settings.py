dffe267b8800922cf4734282bb1d6bc9b5bcb8df
AnaG0307 <ana.genover@gmail.com>
2022-06-06 12:05:52 +0000

    Updated again the AWS S3 region name

--- favbooks_v3/settings.py
+++ favbooks_v3/settings.py
@@ -185,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'favbooks-v3'
-    AWS_S3_REGION_NAME = 'rds.eu-west-2.amazonaws.com'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
